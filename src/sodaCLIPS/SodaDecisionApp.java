5a3c61c39622dc07a09d3d452fc893e35727f4a7
Adam Czyżak <101229929+VadamC27@users.noreply.github.com>
2023-12-11 12:51:34 +0100

    Add class with main function

---
+++ src/sodaCLIPS/SodaDecisionApp.java
@@ -0,0 +1,24 @@
+package sodaCLIPS;
+
+//import javax.swing.*; 
+//import javax.swing.border.*; 
+//import javax.swing.table.*;
+//import java.awt.*; 
+//import java.awt.event.*; 
+
+import java.text.BreakIterator;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.MissingResourceException;
+ 
+//import CLIPSJNI.*;
+
+public class SodaDecisionApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
